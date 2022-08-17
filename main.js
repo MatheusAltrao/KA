236b3ddb0016b39748e2b6d1aa496d8f98ff1813
myauti <myauti.rafa@outlook.com>
2022-08-17 10:50:28 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+/* menu */
+
+const nav = document.getElementById('navbar')
+const close = document.getElementById('icon-close')
+const menu = document.getElementById('icon-menu')
+
+menu && menu.addEventListener('click', () => nav.classList.add('active'))
+close && close.addEventListener('click', () => nav.classList.remove('active'))
+
+const links = document.querySelectorAll(' ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('active')
+  })
+}
+
+/* button back to top */
+window.addEventListener('scroll', function () {
+  backToTop()
+})
+
+function backToTop() {
+  const buttonTop = document.getElementById('backTopButton')
+  if (window.scrollY >= 560) {
+    buttonTop.classList.add('show')
+  } else {
+    buttonTop.classList.remove('show')
+  }
+}
+
+const scrollTop = () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+document.getElementById('backTopButton').onclick = scrollTop
